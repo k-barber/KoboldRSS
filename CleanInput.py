1e80b14c10aa8acf68546f7274e9e9907afe83ae
Kris Barber <kristopher.p.barber@gmail.com>
2019-10-17 19:45:24 -0400

    input/output cleaning

---
+++ CleanInput.py
@@ -0,0 +1,46 @@
+def clean_input(text):
+    """Removes any web formatting & replaces it with regular text
+
+    Parameters:
+    text (str):  The text to have its formatting removed
+
+    Returns:
+    str: Text with formatting removed
+    """
+    text = text.strip()
+    text = text.replace("&amp;", "&")
+    text = text.replace("&gt;", ">")
+    text = text.replace("&lt;", "<")
+    text = text.replace("&nbsp;", " ")
+    text = text.replace("&quot;", '"')
+    text = text.replace("&apos;", "'")
+    text = text.replace("&#62;", ">")
+    text = text.replace("&#60;", "<")
+    text = text.replace("&#160;", " ")
+    text = text.replace("&#34;", '"')
+    text = text.replace("&#39", "'")
+    return text
+
+def dirty_output(text):
+    """Replaces problematic characters with web accessible ones
+
+    Parameters:
+    text (str):  The text to be formatted
+
+    Returns:
+    str: Text with formatting
+    """
+    text = text.replace("&", "&amp;")
+    text = text.replace(">", "&gt;")
+    text = text.replace("<", "&lt;")
+    text = text.replace(" ", "&nbsp;")
+    text = text.replace('"', "&quot;")
+    text = text.replace("'", "&apos;")
+    text = text.replace(">", "&#62;")
+    text = text.replace("<", "&#60;")
+    text = text.replace(" ", "&#160;")
+    text = text.replace('"', "&#34;")
+    text = text.replace("'", "&#39")
+    text = text.replace(":", "~")
+    text = text.replace(" ", "_")
+    return text
