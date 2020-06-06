4bff1c742fd9ccb048717f1ea9dc6d78656d45f8
Kris Barber <kristopher.p.barber@gmail.com>
2020-06-06 17:41:51 -0400

    Properly handle gui updates from Shell

--- Gui.py
+++ Gui.py
@@ -5,16 +5,26 @@ import tkinter.scrolledtext as tkst
 class RSSWindow:
 
     shell = None
+    server_output = None
+    root = None
+
+    def update(self):
+        self.root.update_idletasks()
+        self.root.update()
+
+    def print_to_server_output(self, string):
+        self.server_output.insert(END, str(string) + "\n\r")
+    
     def __init__(self, ShellInstance):
         self.shell = ShellInstance
         print(self.shell.get_value())
-        root = Tk(screenName="RSS Generator")
-        root.title("RSS Generator")
+        self.root = Tk(screenName="RSS Generator")
+        self.root.title("RSS Generator")
 
-        mainframe = ttk.Frame(root, width=300, height=300, padding="5 5 5 5")
+        mainframe = ttk.Frame(self.root, width=300, height=300, padding="5 5 5 5")
         mainframe.grid(row=0, column=0, sticky="news")
-        root.columnconfigure(0, weight=1)
-        root.rowconfigure(0, weight=1)
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
         mainframe.columnconfigure(0, weight=1)
         mainframe.rowconfigure(0, weight=1)
         mainframe.columnconfigure(1, weight=1)
@@ -50,29 +60,37 @@ class RSSWindow:
 
         generator_output.grid(row=0, column=0)
 
-        debug_mode = BooleanVar()
-
         # Set up server output
         server_output_y_scrollbar = Scrollbar(server_output_box)
         server_output_x_scrollbar = Scrollbar(server_output_box, orient=HORIZONTAL)
 
-        server_output = Text(server_output_box, width=30, wrap="none",
+        self.server_output = Text(server_output_box, width=30, wrap="none",
                         xscrollcommand=server_output_x_scrollbar.set,
                         yscrollcommand=server_output_y_scrollbar.set,
                         borderwidth=0, highlightthickness=0, bg="#000000", fg="#FFFFFF", insertbackground="#FFFFFF")
 
-        server_output_y_scrollbar.config(command=server_output.yview)
+        server_output_y_scrollbar.config(command=self.server_output.yview)
         server_output_y_scrollbar.grid(row=0, column=1, sticky=N+S+E+W)
         server_output_x_scrollbar.grid(row=1, column=0, sticky=N+S+E+W)
 
-        server_output_x_scrollbar.config(command=server_output.xview)
+        server_output_x_scrollbar.config(command=self.server_output.xview)
 
-        server_output.grid(row=0, column=0)
+        self.server_output.grid(row=0, column=0)
 
         debug_mode = BooleanVar()
 
-        ttk.Checkbutton(options_box, text="Debug Mode", variable=debug_mode, onvalue=True).grid(row=0, column=0, sticky="news", pady=5, padx=5)
+        ttk.Checkbutton(options_box, text="Debug Mode", variable=debug_mode, onvalue=True).grid(row=0, column=0, sticky="news", pady=5, padx=5, columnspan=2)
+        port = StringVar()
+        Label(options_box, text="Server Options:").grid(row=1, column=0, sticky="news", pady=5, padx=5, columnspan=2)
+        Label(options_box, text="Port:").grid(row=2, column=0, sticky="news")
+        port_number = Entry(options_box, width=5, textvariable=port)
+        port_number.insert(index=0, string="8000")
+        port_number.grid(row=2, column=1, sticky="news", pady=5, padx=5)
 
+        def print_port():
+            self.shell.print_port(port.get())
+
+        ttk.Button(options_box, text="Start Server", command=print_port).grid(row=3, column=0, sticky="news", pady=5, padx=5, columnspan=2)
+        
+        port.set("8000")
         generator_output.insert(END, str(debug_mode.get()))
-
-        root.mainloop()
--- Shell.py
+++ Shell.py
@@ -4,12 +4,16 @@ from Gui import RSSWindow
 class ShellInstance:
 
     value = "twenty"
-
+    gui = None
     #utils = login_utils.Login_Utils(debug_mode.get())
 
     def get_value(self):
         return self.value
 
+    def print_port(self, value):
+        self.gui.print_to_server_output(value)
+
     def __init__(self):
-        gui = RSSWindow(self)
-    
+        self.gui = RSSWindow(self)
+        while True:
+            self.gui.update()
